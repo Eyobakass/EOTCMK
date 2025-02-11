607f6f4c3d4788e523b8c6a28d604240d0eb4988
Eyob <ekassaye0461@gmail.com>
2025-02-11 15:11:17 +0300

    almost finishing the navigation layout

--- src/navigation.jsx
+++ src/navigation.jsx
@@ -1,28 +1,59 @@
 import { useState } from 'react'
-import { FaSearch } from "react-icons/fa"
-import navigation from "./navigation.css"
+import {SearchIcon}  from '@heroicons/react/outline';
 
+
+
+function SmallScreenNav(){
+
+}
+function Logo() {
+    return (
+      <div className="flex items-center space-x-4">
+        {/* Logo Image */}
+        <img src="logo.png" alt="MK-Logo" className="w-24 h-auto ml-4" />
+  
+        {/* Text Content */}
+        <div className="text-left">
+          <h2 className="text-black text-lg font-semibold leading-snug">
+            በኢትዮጵያ ኦርቶዶድስ ተዋህዶ ቤተክርስቲያን <br />
+            በሰንበት ትምህርት ቤቶች ማደራጃ መምሪያ
+          </h2>
+          <h2 className="text-red-600 text-lg font-bold">
+            ማኅበራዊ ጉዳዮች
+          </h2>
+        </div>
+      </div>
+    );
+  }
+  
 function Body() {
   const [count, setCount] = useState(0)
 
   return (
-    <header className='flex'>
-        <img src='image_2025-02-11_10-24-24.png' alt='EOTCMK'className='w-[50vw] sm:w-[100vw]'/>
-        <nav className='margin-auto'>
-            <ul className='flex'>
-                <li className='nowrap-whitespace-nowrap'><a href='#'>ቀዳሚ ገጽ</a></li>
-                <li><a href='#'>ስለ ማኅበሩ</a></li>
-                <li><a href='#'>ትምህርቶች</a></li>
-                <li><a href='#'>ጊቢ ጉባኤ</a></li>
-                <li><a href='#'>ሌሎች ድረ ገጾች</a></li>
-                <li><a href='#'>ቋንቋ</a></li>
-                <li><a href='#'>ያግኙን</a></li>
-            </ul>
-        </nav>
-        <button>
-            <FaSearch className='text-blue-500'/>
-        </button>;
-    </header>
+    <div>
+        <header className="flex justify-between bg-[url('cloud.png')] bg-cover bg-center backdrop-blur-sm h-[15vh]">
+            <Logo/>
+            <nav className='h-[100%]'>
+                <ul className="hidden md:flex space-x-4 text-gray">
+                    <li className='whitespace-nowrap'><a href='#' className=''>ቀዳሚ ገጽ</a></li>
+                    <li className='whitespace-nowrap'><a href='#'>ስለ ማኅበሩ</a></li>
+                    <li className='whitespace-nowrap'><a href='#'>ትምህርቶች</a></li>
+                    <li className='whitespace-nowrap'><a href='#'>ጊቢ ጉባኤ</a></li>
+                    <li className='whitespace-nowrap'><a href='#'>ሌሎች ድረ ገጾች</a></li>
+                    <li className='whitespace-nowrap'><a href='#'>ቋንቋ</a></li>
+                    <li className='whitespace-nowrap'><a href='#'>ያግኙን</a></li>
+                </ul>
+            </nav>
+            <button>
+                <SearchIcon className='text-blue-500'/>
+            </button>;
+            <button></button>
+        </header>
+        <section className='bg-[#295CA4] h-[40vh]'>
+            <h2 className=' bg-[black] bg-opacity-[0.5] bg w-[30vw] m-auto text-center text-3xl text-white'>እንኳን ደህና መጡ!</h2>
+            <img src='logo.png' className='justify-center m-auto  h-[35vh] '/>
+        </section>
+    </div>
   )
 }
 
